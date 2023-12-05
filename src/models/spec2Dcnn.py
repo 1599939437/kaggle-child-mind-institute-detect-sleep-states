0b64ccdb42b59c93d10386053a770a9249ecf446
1599939437 <101985647+1599939437@users.noreply.github.com>
2023-12-05 22:47:16 +0800

    Update spec2Dcnn.py

--- src/models/spec2Dcnn.py
+++ src/models/spec2Dcnn.py
@@ -20,10 +20,12 @@ class Spec2DCNN(BaseModel):
         encoder_weights: Optional[str] = None,
         mixup_alpha: float = 0.5,
         cutmix_alpha: float = 0.5,
+        smp_name: str = 'Unet',
     ):
         super().__init__()
+        self.smp_name = smp_name
         self.feature_extractor = feature_extractor
-        self.encoder = smp.Unet(
+        self.encoder = smp.smp_name(
             encoder_name=encoder_name,
             encoder_weights=encoder_weights,
             in_channels=in_channels,
